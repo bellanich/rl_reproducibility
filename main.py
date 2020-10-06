c9623510e776d97aa68c47e26c2d572eda606254
bellanicholson <nicholsonbella@gmail.com>
2020-10-06 18:25:31 +0200

    Messed around with generating figures. Created filepaths to save best model and figures.

--- main.py
+++ main.py
@@ -9,49 +9,78 @@ import sys
 import gym
 import time
 import argparse
+import os
+import seaborn as sns
+
+# Personal imports.
+from model import NNPolicy
+from utils import run_episodes_policy_gradient, smooth
+sns.set()
+
+"""
+TODO List:
+(1) Figure out if tqdm is necessary.
+(2) Figure out to measure model performance. (What metric do we use?)
+(3) Subtract out baseline from REINFORCE AND GPOMDP. Do additional tricks if necessary to stabilize performance.
+"""
 
 def tqdm(*args, **kwargs):
     return _tqdm(*args, **kwargs, mininterval=1)  # Safety, do not overflow buffer
 # Reminder to activate mini-environment
 assert sys.version_info[:3] >= (3, 6, 0), "Make sure you have Python 3.6 installed!"
 
-# Personal imports.
-from model import NNPolicy
-from utils import run_episodes_policy_gradient, smooth
-
 # Arg parser. This will make it easier when we eventually have to write a bash script to send things to Lisa.
 parser = argparse.ArgumentParser(description='Model Parameters')
-parser.add_option('-n', '--num-episodes', dest='num_episodes', default=500, type='int')
-parser.add_option('-g', '--discount-factor', dest='discount_factor', default=0.99, type='float')
-parser.add_option('-s', '--seed', dest='seed', default=42,  type='int')
-parser.add_option('-l', '--learning-rate', dest='learn_rate', default=0.001, type='float', help='learning rate')
-(options, args) = parser.parse_args()
+parser.add_argument('--num-episodes', dest='num_episodes', default=50, type=int) #500
+parser.add_argument('--discount-factor', dest='discount_factor', default=0.99, type=float)
+parser.add_argument('--seed', dest='seed', default=42,  type=int)
+parser.add_argument('--learning-rate', dest='learn_rate', default=0.001, type=float, help='learning rate')
+parser.add_argument('--hidden-layers', dest='num_hidden', default=128, type=int, help='number of hidden layers')
+args = parser.parse_args()
 
-# Let's sample some episodes
+# List of environment we'll be applying our model to.
 my_envs = ["CartPole-v1", "Acrobot-v1", "MountainCar-v0"]
-env = gym.make(my_envs[2])
 
-# Initialize network.
-# We need to use the shape of our observations matrix to define our input_dim size. This is a hackey way to get it.
-intial_step = env.reset()
-num_hidden = 128
-torch.manual_seed(1234)
-policy = NNPolicy(input_size=intial_step.shape[0],
-                  output_size=env.action_space.n,
-                  num_hidden=num_hidden)
-# Now seed both the environment and network.
-torch.manual_seed(args.seed)
-env.seed(args.seed)
+# Directories to save output files in.
+figures_path, models_path = os.path.join('outputs', 'figures'), os.path.join('outputs', 'models')
+best_performance = {env_name: -1.0 for env_name in my_envs}
+# Initialize figure for learning curve plots.
+fig = plt.figure(1)
+ax = fig.add_subplot(111)
 
-# Simulate N episodes. (Code from lab.)
-episode_durations_policy_gradient = run_episodes_policy_gradient(policy,
-                                                                 env,
-                                                                 args.num_episodes,
-                                                                 args.discount_factor,
-                                                                 args.learn_rate)
+for env_name in my_envs:
+    # Make environment.
+    env = gym.make(env_name)
+    print("Initialize Network for {}.".format(env_name))
+    # Now seed both the environment and network.
+    torch.manual_seed(args.seed)
+    env.seed(args.seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
-# Plot learning curves.
-plt.plot(smooth(episode_durations_policy_gradient, 10))
+    # Finally, initialize network. (Needs to be reinitalized, because input dim size varies with environment.
+    policy = NNPolicy(input_size=env.observation_space.shape[0],
+                      output_size=env.action_space.n,
+                      num_hidden=args.num_hidden)
+
+
+    print("Training for {} episodes.".format(args.num_episodes))
+    # Simulate N episodes. (Code from lab.)
+    episode_durations_policy_gradient = run_episodes_policy_gradient(policy,
+                                                                     env,
+                                                                     args.num_episodes,
+                                                                     args.discount_factor,
+                                                                     args.learn_rate)
+
+    # Plot learning curves.
+    ax.plot(smooth(episode_durations_policy_gradient, 10), label=env_name)
+
+    # Save best policy.
+    # todo: add if statement to check model performannce
+    torch.save(policy.state_dict(), os.path.join(models_path, "{}_model.pt".format(env_name.replace('-','_'))))
+
+# Finally, make legend and save aggregate learning curves plot.
+handles, labels = ax.get_legend_handles_labels()
+lgd = ax.legend(handles, labels, loc='upper center', bbox_to_anchor=(1.2,0.6))
 plt.title('Episode durations per episode')
-plt.legend(['Policy gradient'])
-plt.show()
+plt.savefig(os.path.join(figures_path, 'training_curves'), bbox_extra_artists=(lgd,), bbox_inches='tight')
