ddea8f3cb109d1222823fd44cd3b3e43dc7728a3
bellanicholson <nicholsonbella@gmail.com>
2020-10-13 13:10:18 +0200

    Added more comments.

--- plot_cumulative_rewards.py
+++ plot_cumulative_rewards.py
@@ -21,7 +21,9 @@ save_path = os.path.join('outputs', 'figures', 'cumulative_rewards')
 # Initialize the one figure we'll put all of the plots in.
 fig = plt.figure(1)
 
+# Loop over each average model performance to add it to initalized figure.
 for filename in files_list:
+
     # Load info.
     env_name, model = filename.split('_')[3], " ".join(filename.split('_')[:3]).lower()
     sample_freq = filename.split('_')[-2]
@@ -31,15 +33,15 @@ for filename in files_list:
     # Once policy gradient statistics are calculated, then let's load this here
     # Should replace variance variable.
     variance = policy_rewards.var()
-    # Use sample frequency to recreate the x-axis.
-    episodes = np.arange(policy_rewards.shape[0]) * int(sample_freq)
+    episodes = np.arange(policy_rewards.shape[0]) * int(sample_freq) # Use sample frequency to recreate the x-axis.
 
-    # Putting results into plot.
+    # TODO: remember that we're plotting the AVERAGE cum_rewards over many seeds.
     plt.plot(episodes, policy_rewards, label=model)
     plt.fill_between(episodes,
                     (policy_rewards - variance),
                     (policy_rewards + variance), alpha=0.3)
 
+# Save figure.
 plt.title('Cumulative Rewards')
 plt.legend(bbox_to_anchor=(0.5, -0.15), loc='lower center', ncol=len(files_list), borderaxespad=0.)
 fig.savefig(save_path, bbox_inches='tight')
